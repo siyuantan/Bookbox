7a3cc62e0dc1cc4d1a7a544c79f568d32af62c79
Siyuan Tan <15sic042h@.sit.singaporetech.edu.sg>
2022-05-15 20:50:10 +0800

    initial springboot project setup

---
+++ src/main/java/com/example/bookbox/BookboxApplication.java
@@ -0,0 +1,13 @@
+package com.example.bookbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookboxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/bookbox/BookboxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bookbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookboxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
